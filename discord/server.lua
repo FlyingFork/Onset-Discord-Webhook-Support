09960392f325cd385c99e29f372cfd8009e2ee92
FlyingFork <62627418+FlyingFork@users.noreply.github.com>
2020-06-29 13:21:05 +0300

    Version 1.1
    Added embed support

---
+++ discord/server.lua
@@ -0,0 +1,46 @@
+WebhookURL = "YOUR WEBHOOK LINK HERE; CHECK README TO SEE WHAT TO PUT IN HERE"
+
+function sendDiscordMessage(Message)
+
+    local body = ""
+    body = body.."content="..url_encode(tostring(Message))
+
+    local r = http_create()
+    http_set_host(r, "discordapp.com")
+    http_set_target(r, WebhookURL)
+    http_set_verb(r, "post")
+    http_set_timeout(r, 5000)
+    http_set_field(r, "content-length", string.len(body))
+    http_set_field(r, "content-type", "application/x-www-form-urlencoded; charset=utf-8")
+    
+    http_set_body(r, body)
+	
+    if http_send(r, function() return true end) == false then
+	print("Discord message failed to send")
+	http_destroy(r)
+    end
+end
+AddFunctionExport("sendDiscordMessage", sendDiscordMessage)
+
+function sendDiscordEmbed(theEmbed)
+    jsonTable = jencode({["embeds"] = {theEmbed}})
+	
+	local body = ""
+	body = body .. "payload_json=" .. url_encode(jsonTable)
+
+    local r = http_create()
+	http_set_host(r, "discordapp.com")
+	http_set_target(r, WebhookURL)
+	http_set_verb(r, "post")
+	http_set_timeout(r, 5000)
+    http_set_field(r, "content-length", string.len(body))
+    http_set_field(r, "content-type", "application/x-www-form-urlencoded; charset=utf-8; application/json")
+    
+    http_set_body(r, body)
+	
+	if http_send(r, function() return true end) == false then
+		print("Discord message failed to send")
+		http_destroy(r)
+    end
+end
+AddFunctionExport("sendDiscordEmbed", sendDiscordEmbed)
